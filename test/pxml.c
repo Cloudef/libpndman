cc9dfaa2954ae28a6e00cdfa422cd46d2e121ae0
Cloudef <mailRoxas@gmail.com>
2011-12-27 15:07:53 +0200

    Add win32 test code

--- test/pxml.c
+++ test/pxml.c
@@ -14,23 +14,28 @@
 
 int main()
 {
+#ifdef __linux__
    DIR *dp;
    struct dirent *ep;
+#else
+   WIN32_FIND_DATA dp;
+   HANDLE hFind = NULL;
+#endif
    char path1[PATH_MAX];
    char path2[PATH_MAX];
+   size_t count = 0;
 
    puts("This test, tests various pxml operations within libpndman.");
    puts("");
 
+   if (pndman_init() == -1)
+      return EXIT_FAILURE;
+
+#ifdef __linux__
    /* copy path */
    strncpy(path1, PND_DIR, PATH_MAX-1);
    strncat(path1, "/",     PATH_MAX-1);
 
-   if (pndman_init() == -1)
-      return EXIT_FAILURE;
-
-#if 1
-#ifdef __linux__
    dp = opendir(PND_DIR);
    if (!dp)
       return EXIT_FAILURE;
@@ -43,17 +48,34 @@ int main()
          strncat(path2, ep->d_name, PATH_MAX-1);
          puts(path2);
          pnd_do_something(path2);
+
+         ++count;
       }
    }
 
    closedir(dp);
-#endif
-#else
-   pnd_do_something("/media/Storage/pandora/menu/shootet.mrz.pnd");
+#elif __WIN32__
+    sprintf(path1, "%s/*.pnd", PND_DIR);
+
+    if ((hFind = FindFirstFile(path1, &dp)) == INVALID_HANDLE_VALUE)
+       return EXIT_FAILURE;
+
+    do
+    {
+       sprintf(path2, "%s/%s", PND_DIR, dp.cFileName);
+       puts(path2);
+       pnd_do_something(path2);
+
+       ++count;
+    } while (FindNextFile(hFind, &dp));
+    FindClose(hFind);
 #endif
 
    if (pndman_quit() == -1)
       return EXIT_FAILURE;
+
+   puts("");
+   printf("%zu PNDs\n", count);
 
    return EXIT_SUCCESS;
 }
