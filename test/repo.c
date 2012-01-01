2bc1a9899a24813d98645d8d571cc231e31c68ed
Cloudef <mailRoxas@gmail.com>
2012-01-01 17:12:30 +0200

    Update repository test

--- test/repo.c
+++ test/repo.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include "pndman.h"
 
+#define REPO_URL "http://repo.openpandora.org/includes/get_data.php"
+
 int main()
 {
    pndman_repository repository, *r;
 
    puts("This test, tests various repository operations within libpndman.");
+   puts("We'll first try to add "REPO_URL" two times, but since it already exists, second add should fail.");
    puts("");
    puts("Additionally it does some free trickery, so there should be no memory leaks or segmentation faults either.");
    puts("");
@@ -16,6 +19,17 @@ int main()
 
    /* add repository */
    pndman_repository_init(&repository);
+   pndman_repository_add(REPO_URL, &repository);
+   pndman_repository_add(REPO_URL, &repository);
+
+   /* sync repositories */
+   pndman_repository_sync_all(&repository, NULL);
+
+   puts("");
+   r = &repository;
+   for (; r; r = r->next)
+      printf("%s : %s\n", r->url, r->name);
+   puts("");
 
    /* free everything */
    pndman_repository_free_all(&repository);
