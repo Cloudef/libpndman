d06a916c2ca502643a2d0f0dc06351d82c5d49e0
Cloudef <mailRoxas@gmail.com>
2012-01-01 17:12:20 +0200

    Start of json code

---
+++ lib/json.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "pndman.h"
+#include "package.h"
+#include "repository.h"
+
+/* \brief query repository information from json */
+int _pndman_query_repository_from_json(pndman_repository *repo)
+{
+   return RETURN_OK;
+}
+
+/* vim: set ts=8 sw=3 tw=0 :*/
