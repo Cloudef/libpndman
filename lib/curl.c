68dbf1f10f5cde2fa03f47ac1475abd0201f93fe
Cloudef <mailRoxas@gmail.com>
2012-02-16 19:18:41 +0200

    Add curl request init/free functions Add some asserts as well

--- lib/curl.c
+++ lib/curl.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #include <string.h>
 #include <stdlib.h>
 #include <curl/curl.h>
@@ -10,6 +11,7 @@
 /* \brief write to file */
 size_t curl_write_file(void *data, size_t size, size_t nmemb, FILE *file)
 {
+   assert(file);
    size_t written = fwrite(data, size, nmemb, file);
    return written;
 }
@@ -23,6 +25,7 @@ int curl_progress_func(void* data, double TotalToDownload, double NowDownloaded,
 /* \brief write request */
 size_t curl_write_request(void *data, size_t size, size_t nmemb, curl_request *request)
 {
+   assert(request);
    if (request->result.pos + size * nmemb >= MAX_REQUEST - 1)
       return 0;
 
@@ -36,6 +39,7 @@ size_t curl_write_request(void *data, size_t size, size_t nmemb, curl_request *r
  * NOTE: you must manually set curl pointer to NULL */
 int curl_init_request(curl_request *request)
 {
+   assert(request);
    if (request->curl)   curl_easy_reset(request->curl);
    else request->curl = curl_easy_init();
    if (!request->curl) return RETURN_FAIL;
@@ -45,14 +49,21 @@ int curl_init_request(curl_request *request)
       curl_free_request(request);
       return RETURN_FAIL;
    }
+   memset(request->result.data, 0, MAX_REQUEST);
+   request->result.pos = 0;
+
    return RETURN_OK;
 }
 
 /* \brief free internal curl request */
 void curl_free_request(curl_request *request)
 {
+   assert(request);
    if (request->curl)         curl_easy_cleanup(request->curl);
    if (request->result.data)  free(request->result.data);
+   request->curl        = NULL;
+   request->result.data = NULL;
+   request->result.pos  = 0;
 }
 
 /* vim: set ts=8 sw=3 tw=0 :*/
