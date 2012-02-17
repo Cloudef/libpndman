2d066c351eb13ba3606366d54e0c1fcfd0e09222
Cloudef <mailRoxas@gmail.com>
2012-02-17 17:17:20 +0200

    Complete json parsing

--- lib/json.c
+++ lib/json.c
@@ -11,7 +11,152 @@
 static int _json_set_string(char *string, json_t *object, size_t max)
 {
    if (!object) return RETURN_FAIL;
-   strncpy(string, json_string_value(object), max-1);
+   strncpy(string, json_string_value(object), max);
+   return RETURN_OK;
+}
+
+/* \brief helper int setter */
+static int _json_set_number(double *i, json_t *object)
+{
+   if (!object) return RETURN_FAIL;
+   *i = json_number_value(object);
+   return RETURN_OK;
+}
+
+/* \brief helper version setter */
+static int _json_set_version(pndman_version *ver, json_t *object)
+{
+   char type[PND_VER];
+   if (!object) return RETURN_FAIL;
+   _json_set_string(ver->major,     json_object_get(object,"major"),    PND_VER);
+   _json_set_string(ver->minor,     json_object_get(object,"minor"),    PND_VER);
+   _json_set_string(ver->release,   json_object_get(object,"release"),  PND_VER);
+   _json_set_string(ver->build,     json_object_get(object,"build"),    PND_VER);
+   _json_set_string(type,           json_object_get(object,"type"),     PND_VER);
+   ver->type = !strcmp(type,PND_TYPE_BETA)  ? PND_VERSION_BETA  :
+               !strcmp(type,PND_TYPE_ALPHA) ? PND_VERSION_ALPHA : PND_VERSION_RELEASE;
+   return RETURN_OK;
+}
+
+/* \brief helper author setter */
+static int _json_set_author(pndman_author *author, json_t *object)
+{
+   if (!object) return RETURN_FAIL;
+   _json_set_string(author->name,      json_object_get(object,"name"),     PND_NAME);
+   _json_set_string(author->website,   json_object_get(object,"website"),  PND_STR);
+   _json_set_string(author->email,     json_object_get(object,"email"),    PND_STR);
+   return RETURN_OK;
+}
+
+/* \brief helper previewpic setter */
+static int _json_set_previewpics(pndman_package *pnd, json_t *object)
+{
+   pndman_previewpic *c;
+   json_t *element;
+   unsigned int p;
+
+   if (!object)                  return RETURN_FAIL;
+   if (!json_is_array(object))   return RETURN_FAIL;
+   for (p = 0; p != json_array_size(object); ++p) {
+      element = json_array_get(object, p);
+      if ((c = _pndman_package_new_previewpic(pnd)))
+         _json_set_string(c->src, element, PND_PATH);
+   }
+
+   return RETURN_OK;
+}
+
+/* \brief helper license setter */
+static int _json_set_licenses(pndman_package *pnd, json_t *object)
+{
+   pndman_license *l;
+   json_t *element;
+   unsigned int p;
+
+   if (!object)                  return RETURN_FAIL;
+   if (!json_is_array(object))   return RETURN_FAIL;
+   for (p = 0; p != json_array_size(object); ++p) {
+      element = json_array_get(object, p);
+      if ((l = _pndman_package_new_license(pnd)))
+         _json_set_string(l->name, element, PND_SHRT_STR);
+   }
+
+   return RETURN_OK;
+}
+
+/* \brief helper license source setter (licenses must be set first!) */
+static int _json_set_sources(pndman_package *pnd, json_t *object)
+{
+   pndman_license *l;
+   json_t *element;
+   unsigned int p;
+
+   if (!object)                  return RETURN_FAIL;
+   if (!json_is_array(object))   return RETURN_FAIL;
+   l = pnd->license;
+   for (p = 0; l && p != json_array_size(object); ++p) {
+      element = json_array_get(object, p);
+      _json_set_string(l->sourcecodeurl, element, PND_STR);
+      l = l->next;
+   }
+
+   return RETURN_OK;
+}
+
+/* \brief helper category setter */
+static int _json_set_categories(pndman_package *pnd, json_t *object)
+{
+   pndman_category *c = NULL;
+   json_t *element;
+   unsigned int p;
+
+   if (!object)                  return RETURN_FAIL;
+   if (!json_is_array(object))   return RETURN_FAIL;
+   for (p = 0; p != json_array_size(object); ++p) {
+      element = json_array_get(object, p);
+
+      if (!c) {
+         if ((c = _pndman_package_new_category(pnd)))
+            _json_set_string(c->main, element, PND_SHRT_STR);
+      } else {
+         _json_set_string(c->sub, element, PND_SHRT_STR);
+         c = NULL;
+      }
+   }
+
+   return RETURN_OK;
+}
+
+/* \brief helper localization setter */
+static int _json_set_localization(pndman_package *pnd, json_t *object)
+{
+   const char *key;
+   json_t *element;
+   void *iter;
+   pndman_translated *t;
+
+   if (!object) return RETURN_FAIL;
+   iter = json_object_iter(object);
+   while (iter) {
+      key      = json_object_iter_key(iter);
+      element  = json_object_iter_value(iter);
+
+      /* add title */
+      if ((t = _pndman_package_new_title(pnd))) {
+         strncpy(t->lang, key, PND_SHRT_STR);
+         _json_set_string(t->string, json_object_get(element, "title"), PND_STR);
+      }
+
+      /* add description */
+      if ((t = _pndman_package_new_description(pnd))) {
+         strncpy(t->lang, key, PND_SHRT_STR);
+         _json_set_string(t->string, json_object_get(element, "description"), PND_STR);
+      }
+
+      /* next */
+      iter = json_object_iter_next(object, iter);
+   }
+
    return RETURN_OK;
 }
 
@@ -26,9 +171,10 @@ static int _pndman_json_repo_header(json_t *repo_header, pndman_repository *repo
    _json_set_string(repo->updates, json_object_get(repo_header, "updates"), REPO_URL-1);
    if ((element = json_object_get(repo_header, "version")))
       if (json_is_string(element))
-         strncpy(repo->version, json_string_value(element), REPO_VERSION-1);
+         strncpy(repo->version, json_string_value(element), REPO_VERSION);
       else
          snprintf(repo->version, REPO_VERSION-1, "%.2f", json_number_value(element));
+   _json_set_number((double*)&repo->timestamp, json_object_get(repo_header, "timestamp"));
 
    return RETURN_OK;
 }
@@ -43,16 +189,27 @@ static int _pndman_json_process_packages(json_t *packages, pndman_repository *re
    p = 0;
    for (; p != json_array_size(packages); ++p) {
       package = json_array_get(packages, p);
-      if (!json_is_object(package)) return RETURN_FAIL;
+      if (!json_is_object(package)) continue;
 
       /* INFO READ HERE */
       pnd = _pndman_repository_new_pnd(repo);
       if (!pnd) return RETURN_FAIL;
-      _json_set_string(pnd->id,     json_object_get(package,"id"),     PND_ID);
-      _json_set_string(pnd->icon,   json_object_get(package,"icon"),   PND_PATH);
-      _json_set_string(pnd->md5,    json_object_get(package,"md5"),    PND_MD5);
-      _json_set_string(pnd->vendor, json_object_get(package,"vendor"), PND_NAME);
-      _json_set_string(pnd->url,    json_object_get(package,"uri"),    PND_STR);
+      _json_set_string(pnd->id,           json_object_get(package,"id"),      PND_ID);
+      _json_set_string(pnd->url,          json_object_get(package,"uri"),     PND_STR);
+      _json_set_version(&pnd->version,    json_object_get(package,"version"));
+      _json_set_localization(pnd,         json_object_get(package,"localization"));
+      _json_set_string(pnd->info,         json_object_get(package,"info"),    PND_INFO);
+      _json_set_number((double*)&pnd->size, json_object_get(package, "size"));
+      _json_set_string(pnd->md5,          json_object_get(package,"md5"),     PND_MD5);
+      _json_set_number((double*)&pnd->modified_time, json_object_get(package, "modified_time"));
+      _json_set_number((double*)&pnd->rating, json_object_get(package, "rating"));
+      _json_set_author(&pnd->author,      json_object_get(package,"author"));
+      _json_set_string(pnd->vendor,       json_object_get(package,"vendor"),  PND_NAME);
+      _json_set_string(pnd->icon,         json_object_get(package,"icon"),    PND_PATH);
+      _json_set_previewpics(pnd,          json_object_get(package,"previewpics"));
+      _json_set_licenses(pnd,             json_object_get(package,"licenses"));
+      _json_set_sources(pnd,              json_object_get(package,"source"));
+      _json_set_categories(pnd,           json_object_get(package,"categories"));
    }
    return RETURN_OK;
 }
@@ -114,7 +271,6 @@ int _pndman_json_commit(pndman_repository *r, FILE *f)
       fprintf(f, "\"release\":\"%s\",", p->version.release);
       fprintf(f, "\"build\":\"%s\",", p->version.build);
       fprintf(f, "\"type\":\"%s\"",
-            p->version.type == PND_VERSION_RELEASE ? "release" :
             p->version.type == PND_VERSION_BETA    ? "beta"    :
             p->version.type == PND_VERSION_ALPHA   ? "alpha"   : "release");
       fprintf(f, "},");
