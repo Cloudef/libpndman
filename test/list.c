e7479cf1c6a0cf231d62542ab05cc0db5d4b2765
Cloudef <mailRoxas@gmail.com>
2012-02-19 19:54:17 +0200

    Update list.c

--- test/list.c
+++ test/list.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 #include "pndman.h"
 
 int main()
@@ -11,8 +12,29 @@ int main()
    pndman_device_add("/tmp", &device);
    pndman_device_detect(&device);
 
-   for (d = &device; d; d = d->next)
+   /* print the structure */
+   puts("");
+   for (d = &device; d; d = d->next) {
+      printf("%s : %s\n", d->device, d->mount);
+      printf("next: %s\n", d->next ? d->next->device : "NULL");
+      printf("prev: %s\n", d->prev ? d->prev->device : "NULL");
+   }
+   puts("");
+
+   /* 1 for random order */
+#if 1
+   srand(time(0));
+   while (device.exist) {
+      for (d = &device; (rand() % 2) && d && d->next; d = d->next);
+      printf("free: %s\n", d->device);
       pndman_device_free(d);
+   }
+#else
+   for (d = &device; d; d = d->next) {
+      printf("free: %s\n", d->device);
+      pndman_device_free(d);
+   }
+#endif
 
    return EXIT_SUCCESS;
 }
